45286b308c3044625371dfa6d55314494cbfefe6
PeterZhang2015 <zhangchongzheng@gmail.com>
2025-12-01 17:43:13 +1100

    test: test workflow

--- wizardai_aws_s3_bucket/main.tf
+++ wizardai_aws_s3_bucket/main.tf
@@ -32,19 +32,6 @@ resource "aws_s3_bucket_public_access_block" "this" {
   restrict_public_buckets = true
 }
 
-# Server-side encryption configuration
-resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  bucket = aws_s3_bucket.this.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = var.kms_key_id != null ? "aws:kms" : "AES256"
-      kms_master_key_id = var.kms_key_id
-    }
-    bucket_key_enabled = var.kms_key_id != null ? true : false
-  }
-}
-
 # Versioning configuration
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
