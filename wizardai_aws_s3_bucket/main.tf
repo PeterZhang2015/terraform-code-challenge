d5b6ecd0670e8cf304e1ea7931b43743a07533cd
PeterZhang2015 <zhangchongzheng@gmail.com>
2025-12-01 18:07:42 +1100

    test: test pull request for successful workflows

--- wizardai_aws_s3_bucket/main.tf
+++ wizardai_aws_s3_bucket/main.tf
@@ -22,7 +22,7 @@ resource "aws_s3_bucket" "this" {
   })
 }
 
-# Block all public access
+# Block all public access - testing
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
