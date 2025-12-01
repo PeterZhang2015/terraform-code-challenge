c4f95dea3c8f4c9c0a42b6406a20cd120839ba7d
PeterZhang2015 <zhangchongzheng@gmail.com>
2025-12-01 17:55:38 +1100

    Update testing code

--- wizardai_aws_s3_bucket/main.tf
+++ wizardai_aws_s3_bucket/main.tf
@@ -27,9 +27,22 @@ resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+# Server-side encryption configuration
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = var.kms_key_id != null ? "aws:kms" : "AES256"
+      kms_master_key_id = var.kms_key_id
+    }
+    bucket_key_enabled = var.kms_key_id != null ? true : false
+  }
 }
 
 # Versioning configuration
